d632ee7fd4df9474d73e796f17e872587198fd63
Bryan <bryan20031714@gmail.com>
2024-05-26 22:18:41 -0600

    cambios en models

--- SERVER_ESCOLAR/Migrations/UsuariosContextModelSnapshot.cs
+++ SERVER_ESCOLAR/Migrations/UsuariosContextModelSnapshot.cs
@@ -23,21 +23,14 @@ namespace SERVER_ESCOLAR.Migrations
 
             modelBuilder.Entity("SERVER_ESCOLAR.Models.Usuario", b =>
                 {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<string>("UsuarioId")
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)");
 
                     b.Property<string>("Apellidos")
                         .IsRequired()
                         .HasMaxLength(255)
                         .HasColumnType("nvarchar(255)");
-
-                    b.Property<string>("Ciudad")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
 
                     b.Property<string>("Contrasena")
                         .IsRequired()
@@ -51,20 +44,42 @@ namespace SERVER_ESCOLAR.Migrations
 
                     b.Property<string>("Direccion")
                         .IsRequired()
-                        .HasMaxLength(500)
-                        .HasColumnType("nvarchar(500)");
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
+
+                    b.Property<string>("Dni")
+                        .IsRequired()
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)");
+
+                    b.Property<int>("IdRol")
+                        .HasColumnType("int");
+
+                    b.Property<int>("IdSequencia")
+                        .HasColumnType("int");
+
+                    b.Property<int>("IdUsuario")
+                        .HasColumnType("int");
 
                     b.Property<string>("Nombres")
                         .IsRequired()
                         .HasMaxLength(255)
                         .HasColumnType("nvarchar(255)");
 
-                    b.Property<string>("Rol")
+                    b.Property<string>("Sexo")
                         .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasMaxLength(1)
+                        .HasColumnType("nvarchar(1)");
 
-                    b.HasKey("Id");
+                    b.Property<string>("Telefono")
+                        .IsRequired()
+                        .HasMaxLength(9)
+                        .HasColumnType("nvarchar(9)");
+
+                    b.Property<int>("UsuarioBloqueado")
+                        .HasColumnType("int");
+
+                    b.HasKey("UsuarioId");
 
                     b.HasIndex("Correo")
                         .IsUnique();
---
+++ SERVER_ESCOLAR/Models/ParamRangoNota.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace SERVER_ESCOLAR.Models
+{
+    public class ParamRangoNota
+    {
+        public int IdRangoNota{ get; set; }
+        public int RangoInicial { get; set; }
+        public int RangoFinal{ get; set; }
+        public int IdSequencia{ get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo Observaciones debe tener maximo 50 caracteres ")]
+        [MinLength(5, ErrorMessage = "El campo Observaciones debe tener menos de 5 caracteres")]
+        public string Observaciones { get; set; } = null!;
+
+}
+}
